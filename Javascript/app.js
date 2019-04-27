a8c4fe6e1838900a7d5ed4a9b6c2f401328b5dad
aine <aineekohh@yahoo.com>
2019-04-26 23:59:16 -0500

    initializing firebase

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,1 +1,19 @@
 
+// SET UP 
+  // Initialize Firebase
+
+
+     // Initialize Firebase
+     var config = {
+      apiKey: "AIzaSyB9jy8tZ6CzRAbLhuc8s4nBJk4sW8gJU_c",
+      authDomain: "trainscheduler-b11f4.firebaseapp.com",
+      databaseURL: "https://trainscheduler-b11f4.firebaseio.com",
+      projectId: "trainscheduler-b11f4",
+      storageBucket: "trainscheduler-b11f4.appspot.com",
+      messagingSenderId: "232356613633"
+    };
+    firebase.initializeApp(Config);
+  
+    var name = document.getElementById('name');
+    var dbRef = firebase.database().ref().child('text');
+    dbRef.on('value', snap => name.innerText = snap.val());
